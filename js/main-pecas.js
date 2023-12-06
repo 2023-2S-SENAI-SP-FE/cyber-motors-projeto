4002a8e2af2b74dfd1eeebeebfd0f751e26053fe
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2023-12-06 17:22:13 +0000

    fix: tentando arrumar os erros do icone de coração

--- js/main-pecas.js
+++ js/main-pecas.js
@@ -3,3 +3,16 @@ document.addEventListener('click', event => {
         event.target.classList.toggle('favoritado');
     }
 });
+
+var iconeAntes = "./img/icone-coração-vazio.svg"
+var iconeDepois = "./img/icone-coração-cheio.svg"
+
+function trocar() {
+    var favorito = document.getElementById(logo-coracao);
+
+    if (favorito.src.includes(iconeAntes)) {
+        favorito.src = iconeDepois;
+    } else {
+        favorito.src = iconeAntes;
+    }
+}
