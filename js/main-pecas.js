693ba4c6e359d9566a3ebb19dfe93bf68278ec15
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2023-12-07 17:54:59 +0000

    feat: conclusão da interação do icone de coração

--- js/main-pecas.js
+++ js/main-pecas.js
@@ -1,11 +1,31 @@
 document.addEventListener('click', event => {
-    let iconeAntes = "img/icone-coracao-vazio.svg"
-    let iconeDepois = "img/icone-coracao-cheio.svg"
 
-    if(event.target.classList.contains('iconeAntes')) {
-        event.target.classList.toggle('iconeDepois');
+    if (event.target.classList.contains('coracao')) {
+
+        let iconeAntes = "img/icone-coracao-vazio.svg"
+        let iconeDepois = "img/icone-coracao-cheio.svg"
+
+        if (event.target.src.includes(iconeAntes)) {
+            event.target.src = iconeDepois
+        } else {
+            event.target.src = iconeAntes
+        }
     }
-});
+})
+
+// document.addEventListener('click', event => {
+//     let iconeAntes = "img/icone-coracao-vazio.svg"
+//     let iconeDepois = "img/icone-coracao-cheio.svg"
+//     let favorito = document.getElementById('logo-coracao');
+
+//     if(event.target.classList.contains('coracao')) {
+//         favorito.src = iconeAntes
+//         event.target.toggle('iconeDepois');
+//     } else {
+//         favorito.src = iconeDepois
+//         event.target.toggle('iconeAntes');
+//     }
+// });
 
 // function trocar() {
 //     let favorito = document.getElementById('logo-coracao');
