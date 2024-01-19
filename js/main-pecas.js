8daa8a30141ee310857a9010820142dc8fba5de1
Fabio42-sys <>
2024-01-19 14:10:54 -0300

    fix: js

--- js/main-pecas.js
+++ js/main-pecas.js
@@ -2,11 +2,7 @@ document.addEventListener('click', event => {
 
     if (event.target.classList.contains('coracao')) {
 
-<<<<<<< HEAD
         let iconeAntes = "img/icone-coracao-branco.svg"
-=======
-        let iconeAntes = "img/coracao-pecas.png"
->>>>>>> 2735d3946219833339c67c509488ad4eefc901d7
         let iconeDepois = "img/icone-coracao-cheio.svg"
 
         if (event.target.src.includes(iconeAntes)) {
