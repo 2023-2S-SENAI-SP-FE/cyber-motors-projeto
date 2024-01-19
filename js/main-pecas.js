2149101015f1ffcb85ce8cc3edf2e307c09718b0
Fabio42-sys <>
2024-01-19 13:49:18 -0300

    feat: ultimos arranjos para a apresentação

--- js/main-pecas.js
+++ js/main-pecas.js
@@ -2,7 +2,7 @@ document.addEventListener('click', event => {
 
     if (event.target.classList.contains('coracao')) {
 
-        let iconeAntes = "img/icone-coracao-vazio.svg"
+        let iconeAntes = "img/icone-coracao-branco.svg"
         let iconeDepois = "img/icone-coracao-cheio.svg"
 
         if (event.target.src.includes(iconeAntes)) {
